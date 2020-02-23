0987c5893c8f26445976eb6702b15ae112f2ef69
Nirmal Dahal <16473567+TheNittam@users.noreply.github.com>
2020-02-23 22:32:06 +0545

    Update index.php

--- RegeXSS/RegeXSS/index.php
+++ RegeXSS/RegeXSS/index.php
@@ -4,7 +4,7 @@ if (!file_exists("sender.js")) {
   fwrite($senderJS, "");
   fclose($senderJS);
 }
-?>;?>
+?>
 
   <div class="text-center text-white h-100 align-items-center d-flex py-5" style="background-image: linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url(&quot;img/cover.jpg&quot;); background-position: center center, center center; background-size: cover, cover; background-repeat: repeat, repeat; background-attachment: fixed;">
 
