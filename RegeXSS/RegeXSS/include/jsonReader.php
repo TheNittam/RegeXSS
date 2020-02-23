ac00c0349ce33663e64cd93c8f1c9aebf65ddd49
Nirmal Dahal <16473567+TheNittam@users.noreply.github.com>
2020-02-23 22:06:50 +0545

    Invalid JSON Response Issue Fixed

--- RegeXSS/RegeXSS/include/jsonReader.php
+++ RegeXSS/RegeXSS/include/jsonReader.php
@@ -3,11 +3,13 @@ header("X-Frame-Options: sameorigin");
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$readJSON = file_get_contents('regexss.json');
-
-if(strlen($readJSON) == 0){
-	echo "[]";
-}else{
+if (!file_exists("regexss.json")) {
+	$regJSON = fopen("regexss.json", "w");
+	fwrite($regJSON, "[]");
+	fclose($regJSON);
+	$readJSON = file_get_contents('regexss.json');
 	echo $readJSON;
+}else{
+	echo "[]";
 }
 ?>
